94a4e05dc74467423502e9311ccd1401244988de
daeseong <zang5989@gmail.com>
2024-04-01 17:48:35 +0900

    springBookStudy(학원 공부) 313p REST API: GET 구현하기부터 시작할 것.

--- firstproject1/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject1/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,7 @@ import lombok.ToString;
 @Getter
 public class Article {
     @Id // Entity 대표 값
-    @GeneratedValue // 자동 생성 기능 추가(자동 채번)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성 기능 추가(자동 채번)
     private Long id;
     @Column // title 필드 선언 (열)
     private String title;
